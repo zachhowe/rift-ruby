b182959f362f7a947b665a341b149dcfe7865151
Zach Howe <zach@zachhowe.com>
2013-01-02 14:20:19 -0800

    fixed line endings issue on test_get_url.rb

--- tests/rift_client/test_get_url.rb
+++ tests/rift_client/test_get_url.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-
 require 'nokogiri'
 
 def testDocument(doc)
@@ -14,13 +13,24 @@ def testDocument(doc)
       end
     end
   end
+
+  nil
 end
 
 doc1 = Nokogiri::HTML::Document.parse(File.new('data/test1.html'))
-puts "HTML Document 1 result: " + testDocument(doc1).to_s
+aresult1 = testDocument(doc1).to_s
+eresult1 = '/chatservice/scratch/matchthree?characterId=214483932618244823&replayUUID=015a9a07-1b71-43e8-ae4b-c49fe9bce7b8'
+puts "HTML Document 1 expected result: " + eresult1
+puts "HTML Document 1 actual result: " + aresult1
 
 doc2 = Nokogiri::HTML::Document.parse(File.new('data/test2.html'))
-puts "HTML Document 2 result: " + testDocument(doc2).to_s
+aresult2 = testDocument(doc2).to_s
+eresult2 = ''
+puts "HTML Document 2 expected result: " + eresult2
+puts "HTML Document 2 actual result: " + aresult2
 
 doc3 = Nokogiri::HTML::Document.parse(File.new('data/test3.html'))
-puts "HTML Document 3 result: " + testDocument(doc3).to_s
+aresult3 = testDocument(doc3).to_s
+eresult3 = '/chatservice/scratch/redeem?game=75ac5589-da57-437a-8af6-05715c96b998'
+puts "HTML Document 3 expected result: " + eresult3
+puts "HTML Document 3 actual result: " + aresult3
